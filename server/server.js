97c1a9203f0c3c6856992e81f9f2c601a882105c
Javier <casaja@gmail.com>
2019-02-18 20:29:30 +0100

    Login normal sección 10

--- server/server.js
+++ server/server.js
@@ -16,7 +16,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.use(require('./routes/usuarios'));
+// Configuración global de rutas
+app.use(require('./routes/index'));
 
 mongoose.connect(process.env.URLDB, options, (err, res) => {
     if (err) throw err;
