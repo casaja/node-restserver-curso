d18b37afbbf3620822b770e29102550659882037
Javier SC <casaja@gmail.com>
2019-05-25 18:21:30 +0200

    Productos y categorías

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,5 +4,7 @@ const app = express();
 
 app.use(require('./usuarios'));
 app.use(require('./login'));
+app.use(require('./categorias'));
+app.use(require('./productos'));
 
 module.exports = app;
--- server/routes/usuarios.js
+++ server/routes/usuarios.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const _ = require('underscore');
 const Usuario = require('../models/usuarios');
 const { verificaToken, verificaAdmin_Role } = require('../middlewares/autenticacion');
